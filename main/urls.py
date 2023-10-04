4aeb124e0fe78fafb86a9af9d925646e047a65ca
Andhika Reihan Hervito <andhika.reihan@ui.ac.id>
2023-10-04 13:39:44 +0700

    tutorial 5

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from main.views import register, login_user, logout_user, show_main, create_product, edit_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import get_product_json, add_product_ajax, register, login_user, logout_user, show_main, create_product, edit_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 def register(request):
     form = UserCreationForm()
@@ -56,6 +57,24 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
 
 def create_product(request):
     form = ProductForm(request.POST or None)
